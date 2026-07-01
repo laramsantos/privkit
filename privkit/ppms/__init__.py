6e4fc6f49d7795548da47b03c4d69664686078b2
laramsantos <larasantosxx@gmail.com>
2026-07-01 21:35:52 +0100

    Add Hidden Markov Model Trajectory Prediction Method

--- privkit/ppms/__init__.py
+++ privkit/ppms/__init__.py
@@ -1,7 +1,6 @@
 from .ppm import PPM
 from .planar_laplace import PlanarLaplace
 from .adaptive_geo_ind import AdaptiveGeoInd
-from .gem import GEM
 from .gem_adap_eps import GEM_with_adaptive_epsilon
 from .tppm_hmm import TrajectoryPrivacyProtectionHMM
 from .clustering_prediction import ClusteringPrediction
@@ -22,7 +21,6 @@ __all__ = [
     "PPM",
     "PlanarLaplace",
     "AdaptiveGeoInd",
-    "GEM_with_adaptive_epsilon",
     "TrajectoryPrivacyProtectionHMM",
     "ClusteringPrediction",
     "Hash",
